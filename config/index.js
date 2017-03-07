ff6842c13c33121cf41da0fafadc36dc920600dd
严俊东 <1172482914@qq.com>
2017-03-07 20:20:39 +0800

    打包上线

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../index.html'),
+    assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'lib',
-    assetsPublicPath: '/vue',
+    assetsPublicPath: '/vue-advanced-query/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
